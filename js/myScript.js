b2059059144de8811e1705770eabe391fad0f132
bdbHedgeh0g <98036688+bdbHedgeh0g@users.noreply.github.com>
2022-02-26 19:20:44 +0300

    Add files via upload

---
+++ js/myScript.js
@@ -0,0 +1,9 @@
+
+
+let option = prompt("Какой тип сайта вы хотите? 0-Сайт-визитка; 1-Портал; 2-Магазин; 3-Каталог; 4-Промо; 5-Другие");
+console.log(option);
+
+if option === 0{
+
+}
+<div class="uCalc_392118"></div><script> var widgetOptions392118 = { bg_color: "transparent" }; (function() { var a = document.createElement("script"), h = "head"; a.async = true; a.src = (document.location.protocol == "https:" ? "https:" : "http:") + "//ucalc.pro/api/widget.js?id=392118&t="+Math.floor(new Date()/18e5); document.getElementsByTagName(h)[0].appendChild(a) })();</script>
